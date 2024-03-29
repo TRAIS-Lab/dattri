7fe474b69f95c0250ae6069e6824b0f602e9130c
theaperdeng <theaperdeng@outlook.com>
2024-03-28 21:52:09 -0500

    add a new test

--- test/dattri/func/test_ihvp.py
+++ test/dattri/func/test_ihvp.py
@@ -11,7 +11,7 @@ import torch
 
 class Test_IVP:
 
-    def test_ihvp_easy_cache(self):
+    def test_ihvp_direct(self):
 
         def target(x):
             return torch.sin(x).sum()
@@ -21,4 +21,17 @@ class Test_IVP:
         ihvp = ihvp_direct(target, x, argnums=0)
 
         assert torch.allclose(ihvp(vec), (torch.diag(-1 / x.sin()) @ vec.T).T)
+        assert ihvp(vec).shape == (5, 2)
+    
+    def test_ihvp_direct_argnum(self):
+
+        def target(x, y):
+            return torch.sin(x + y).sum()
+
+        x = 2
+        y = torch.randn(2)
+        vec = torch.randn(5, 2)
+        ihvp = ihvp_direct(target, x, y, argnums=1)
+
+        assert torch.allclose(ihvp(vec), (torch.diag(-1 / (2+y).sin()) @ vec.T).T)
         assert ihvp(vec).shape == (5, 2)
