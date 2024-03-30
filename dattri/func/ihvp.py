c5435d72f558b66827389ebae589e27e82daadcf
theaperdeng <theaperdeng@outlook.com>
2024-03-30 16:39:52 -0500

    add hvp and ihvp_cg

--- dattri/func/ihvp.py
+++ dattri/func/ihvp.py
@@ -11,9 +11,63 @@ if TYPE_CHECKING:
     from collections.abc import Callable
     from typing import Tuple, Union
 
+from functools import partial
+
 import torch
 from torch import Tensor
-from torch.func import hessian
+from torch.func import hessian, grad, vjp, jvp
+
+
+def hvp(func: Callable,
+        primals: Tuple[torch.Tensor, ...],
+        tangents: torch.Tensor,
+        argnums: int = 0,
+        mode: str = "rev-only"):
+    """
+    Hessian Vector Product(HVP) calculation function.
+
+    This function returns a function that could calculate the hessian with
+    respect to the argument user specified.
+
+    :param func: A Python function that takes one or more arguments.
+           Must return a single-element Tensor. The hessian will
+           be calculated on this function.
+    :param primals: Positional arguments to func that must all be Tensors.
+           The returned function will also be computing the derivative with
+           respect to these arguments
+    :param tangents: The “vector” for which Hessian Vector Product is computed.
+    :param argnums: An integer default to 0. Specifies arguments to compute
+           gradients with respect to.
+    :param mode: A string default to `rev-only`. Specifies how to calculate
+           hvp function. Either `rev-only` or `rev-fwd` should be put here.
+    """
+    if mode not in ["rev-only", "rev-fwd"]:
+        raise IHVPUsageException("`mode` should be either"
+                                 "'rev-only' or 'rev-fwd'.")
+
+    if mode == "rev-only":
+        # pylint: disable=unbalanced-tuple-unpacking
+        _, vjp_fn = vjp(grad(func, argnums=argnums), *primals)
+        return vjp_fn(tangents)[argnums]
+    else:
+        # patch the tangents, make zero for other argnums
+        # e.g.,
+        # if argnums = 1, and len(primals) = 3, then it should be patched to
+        # [torch.zeros(*primals[0].shape),
+        # tangents,
+        # torch.zeros(*primals[2].shape)]
+        if len(primals) > 1:
+            tangents_patched = []
+            for i in range(len(primals)):
+                if i != argnums:
+                    tangents_patched.append(torch.zeros(*primals[i].shape))
+                else:
+                    tangents_patched.append(tangents)
+            tangents_patched = tuple(tangents_patched)
+        else:
+            tangents_patched = (tangents,)
+
+        return jvp(grad(func, argnums=argnums), primals, tangents_patched)[1]
 
 
 def ihvp_explicit(func: Callable,
@@ -58,3 +112,71 @@ def ihvp_explicit(func: Callable,
         return torch.linalg.solve(hessian_tensor, vec.T).T
 
     return _ihvp_direct_func
+
+
+def ihvp_cg(func: Callable,
+            *args,
+            argnums: int = 0,
+            max_iter: int = 100,
+            tol: float = 1e-7,
+            mode: str = "rev-only"):
+    """
+    Conjugate Gradient Descent ihvp algorithm function.
+
+    Standing for the inverse-hessian-vector product, returns a function that,
+    when given vectors, computes the product of inverse-hessian and vector.
+
+    Conjugate Gradient Descent algorithm calcualte the hvp function and use
+    it iteratively through Conjugate Gradient
+
+    :param func: A Python function that takes one or more arguments.
+           Must return a single-element Tensor. The hessian will
+           be calculated on this function.
+    :param argnums: An integer default to 0. Specifies arguments to compute
+           gradients with respect to.
+    :param max_iter: An integer default 100. Specifies the maximum iteration
+           to calculate the ihvp.
+    :param tol: A float default to 1e-7. Specifies the break condition that
+           decide if the algorithm has converged.
+    :param mode: A string default to `rev-only`. Specifies how to calculate
+           hvp function. Either `rev-only` or `rev-fwd` should be put here.
+    """
+    hvp_helper_func = partial(hvp, func, (*args, ), argnums=argnums, mode=mode)
+
+    def _ihvp_cg(vec: Tensor):
+        # algorithm refer to
+        # https://www.cs.cmu.edu/~pradeepr/convexopt/Lecture_Slides/conjugate_direction_methods.pdf
+
+        if vec.ndim < 2:
+            vec = vec.unsqueeze(0)
+        batch_ihvp_cg = []
+
+        for i in range(vec.shape[0]):
+            x_pre = torch.clone(vec[i, :])
+            x = x_pre
+            g_pre = vec[i, :] - hvp_helper_func(x)
+            d = d_pre = g_pre
+
+            for _ in range(max_iter):
+                if torch.norm(g_pre) < tol:
+                    break
+                ad = hvp_helper_func(d)
+                alpha = torch.dot(g_pre, d_pre) / torch.dot(d, ad)
+                x += alpha * d
+                g = g_pre - alpha * ad
+
+                beta = torch.dot(g, g) / torch.dot(g_pre, g_pre)
+
+                g_pre = d_pre = g
+                d = g + beta * d
+            batch_ihvp_cg.append(x)
+
+        return torch.stack(batch_ihvp_cg)
+
+    return _ihvp_cg
+
+
+class IHVPUsageException(Exception):
+    """The usage exception class for ihvp module."""
+
+    pass
--- test/dattri/func/test_ihvp.py
+++ test/dattri/func/test_ihvp.py
@@ -1,7 +1,7 @@
 """Unit test for ihvp calculator."""
 import torch
 
-from dattri.func.ihvp import ihvp_explicit
+from dattri.func.ihvp import ihvp_explicit, hvp, ihvp_cg
 
 
 class TestIHVP:
@@ -32,4 +32,91 @@ class TestIHVP:
         ihvp = ihvp_explicit(_target, x, y, argnums=1)
 
         assert torch.allclose(ihvp(vec), (torch.diag(-1 / (2+y).sin()) @ vec.T).T)
+        assert ihvp(vec).shape == (5, 2)
+    
+    def test_hvp_rev_only(self):
+
+        def target(x):
+            return torch.sin(x).sum()
+        
+        x = torch.randn(2)
+        vec = torch.randn(2)
+
+        assert torch.allclose(hvp(target, (x,), vec, argnums=0, mode="rev-only"),
+                             (torch.diag(-x.sin()) @ vec.T).T)
+
+    def test_hvp_rev_fwd(self):
+
+        def target(x):
+            return torch.sin(x).sum()
+        
+        x = torch.randn(2)
+        vec = torch.randn(2)
+
+        assert torch.allclose(hvp(target, (x,), vec, argnums=0, mode="rev-fwd"),
+                             (torch.diag(-x.sin()) @ vec.T).T)
+
+    def test_hvp_rev_only_argnums(self):
+
+        def target(x, y):
+            return torch.sin(x + y).sum()
+
+        x = torch.Tensor([2])
+        y = torch.randn(2)
+        vec = torch.randn(2)
+
+        assert torch.allclose(hvp(target, (x, y), vec, argnums=1, mode="rev-only"),
+                              (torch.diag(-(2+y).sin()) @ vec.T).T)
+
+    def test_hvp_rev_only_argnums(self):
+
+        def target(x, y):
+            return torch.sin(x + y).sum()
+
+        x = torch.Tensor([2])
+        y = torch.randn(2)
+        vec = torch.randn(2)
+
+        assert torch.allclose(hvp(target, (x, y), vec, argnums=1, mode="rev-fwd"),
+                              (torch.diag(-(2+y).sin()) @ vec.T).T)
+        
+    def test_hvp_vmap(self):
+
+        def target(x, y):
+            return torch.sin(x + y).sum()
+
+        x = torch.Tensor([2])
+        y = torch.randn(2)
+        vec = torch.randn(2)
+
+        from functools import partial
+        from torch.func import vmap
+
+        vmap_hvp = partial(hvp, target, (x, y), argnums=1, mode="rev-only")
+
+        assert torch.allclose(vmap(vmap_hvp)(torch.stack([vec for _ in range(5)])),
+                              torch.stack([(torch.diag(-(2+y).sin()) @ vec.T).T for _ in range(5)]))
+    
+    def test_ihvp_cg(self):
+        def target(x):
+            return torch.sin(x).sum()
+
+        x = torch.randn(2)
+        vec = torch.randn(5, 2)
+        ihvp = ihvp_cg(target, x, argnums=0)
+
+        assert torch.allclose(ihvp(vec), (torch.diag(-1 / x.sin()) @ vec.T).T)
+        assert ihvp(vec).shape == (5, 2)
+
+    def test_ihvp_cg_argnum(self):
+
+        def target(x, y):
+            return torch.sin(x + y).sum()
+
+        x = torch.Tensor([1])
+        y = torch.randn(2)
+        vec = torch.randn(5, 2)
+        ihvp = ihvp_cg(target, x, y, argnums=1)
+
+        assert torch.allclose(ihvp(vec), (torch.diag(-1 / (1+y).sin()) @ vec.T).T)
         assert ihvp(vec).shape == (5, 2)
