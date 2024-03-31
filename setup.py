0fa20a3f36144f54260bacbc96ae33f40ef4b9ae
Jiaqi Ma <jiaqima@users.noreply.github.com>
2024-03-31 01:42:20 -0400

    switch {pycodestlye, pydocstyle, pylint} to ruff (#7)
    * switch {pycodestlye, pydocstyle, pylint} to ruff; fixing code styles errors identified by the new ruff check
    
    * remove .pylintrc and fix pyporject.toml

--- setup.py
+++ setup.py
@@ -1,36 +1,40 @@
-#!/usr/bin/env python
+"""Setup."""
+from pathlib import Path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-long_description = '''
+long_description = """
 DAttri
-'''
+"""
 
 exclude_patterns = ["*__pycache__*", "*ipynb_checkpoints*"]
-VERSION = open("version.txt", 'r').read().strip()
+with Path("version.txt").open() as version_file:
+    VERSION = version_file.read().strip()
+
 
 def setup_package():
-    metadata = dict(
-        name='dattri',
-        version=VERSION,
-        description='dattri',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        author='TARIS-lab',
-        url='https://github.com/TRAIS-Lab/dattri',
-        packages=find_packages(),
-        install_requires=[],
-        include_package_data=True,
-        classifiers=[
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: Implementation :: CPython'],
-        platforms=['mac', 'linux', 'windows']
-    )
+    """Set up the package."""
+    metadata = {
+        "name": "dattri",
+        "version": VERSION,
+        "description": "dattri",
+        "long_description": long_description,
+        "long_description_content_type": "text/markdown",
+        "author": "TARIS-Lab",
+        "url": "https://github.com/TRAIS-Lab/dattri",
+        "packages": find_packages(),
+        "install_requires": [],
+        "include_package_data": True,
+        "classifiers": [
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: Implementation :: CPython"],
+        "platforms": ["mac", "linux", "windows"],
+    }
 
     setup(**metadata)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
