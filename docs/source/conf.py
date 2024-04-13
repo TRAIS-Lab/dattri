844f7d9dab12ade9e57785de38a3fce2fee69e73
Xingjian Zhang <jimmyzxj@umich.edu>
2024-04-13 14:23:17 -0400

    Use new theme `furo`.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('..')
+sys.path.append('../..')
 import dattri
 
 # Configuration file for the Sphinx documentation builder.
@@ -27,5 +27,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
