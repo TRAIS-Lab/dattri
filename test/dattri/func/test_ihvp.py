60799b3741c3fbe008345cff28e226c46a8c1bad
theaperdeng <theaperdeng@outlook.com>
2024-04-25 11:47:44 -0500

    relax the test tol

--- test/dattri/func/test_ihvp.py
+++ test/dattri/func/test_ihvp.py
@@ -46,10 +46,14 @@ class TestIHVP:
         x = torch.randn(2)
         vec = torch.randn(2)
 
-        assert torch.allclose(hvp_at_x(target, (x,), argnums=0, mode="rev-rev")(vec),
-                             (torch.diag(-x.sin()) @ vec.T).T)
-        assert torch.allclose(hvp_at_x(target, (x,), argnums=0, mode="rev-fwd")(vec),
-                             (torch.diag(-x.sin()) @ vec.T).T)
+        assert torch.allclose(
+            hvp_at_x(target, (x,), argnums=0, mode="rev-rev")(vec),
+            (torch.diag(-x.sin()) @ vec.T).T,
+        )
+        assert torch.allclose(
+            hvp_at_x(target, (x,), argnums=0, mode="rev-fwd")(vec),
+            (torch.diag(-x.sin()) @ vec.T).T,
+        )
 
     def test_hvp_at_x_argnums(self):
         """Test argnums of hvp_at_x."""
@@ -61,10 +65,14 @@ class TestIHVP:
         y = torch.randn(2)
         vec = torch.randn(2)
 
-        assert torch.allclose(hvp_at_x(target, (x, y), argnums=1, mode="rev-rev")(vec),
-                              (torch.diag(-(2 + y).sin()) @ vec.T).T)
-        assert torch.allclose(hvp_at_x(target, (x, y), argnums=1, mode="rev-rev")(vec),
-                              (torch.diag(-(2 + y).sin()) @ vec.T).T)
+        assert torch.allclose(
+            hvp_at_x(target, (x, y), argnums=1, mode="rev-rev")(vec),
+            (torch.diag(-(2 + y).sin()) @ vec.T).T,
+        )
+        assert torch.allclose(
+            hvp_at_x(target, (x, y), argnums=1, mode="rev-rev")(vec),
+            (torch.diag(-(2 + y).sin()) @ vec.T).T,
+        )
 
     def test_hvp_at_x_vmap(self):
         """Test vmap usage on hvp_at_x."""
@@ -78,10 +86,10 @@ class TestIHVP:
 
         hvp_at_x_func = hvp_at_x(target, (x, y), argnums=1, mode="rev-rev")
 
-        assert torch.allclose(vmap(hvp_at_x_func)(torch.stack([vec for _ in range(5)])),
-                              torch.stack([
-                                  (torch.diag(-(2 + y).sin()) @ vec.T).T
-                                    for _ in range(5)]))
+        assert torch.allclose(
+            vmap(hvp_at_x_func)(torch.stack([vec for _ in range(5)])),
+            torch.stack([(torch.diag(-(2 + y).sin()) @ vec.T).T for _ in range(5)]),
+        )
 
     def test_hvp(self):
         """Test hvp."""
@@ -92,10 +100,14 @@ class TestIHVP:
         x = torch.randn(2)
         vec = torch.randn(2)
 
-        assert torch.allclose(hvp(target, argnums=0, mode="rev-rev")((x,), vec),
-                             (torch.diag(-x.sin()) @ vec.T).T)
-        assert torch.allclose(hvp(target, argnums=0, mode="rev-fwd")((x,), vec),
-                             (torch.diag(-x.sin()) @ vec.T).T)
+        assert torch.allclose(
+            hvp(target, argnums=0, mode="rev-rev")((x,), vec),
+            (torch.diag(-x.sin()) @ vec.T).T,
+        )
+        assert torch.allclose(
+            hvp(target, argnums=0, mode="rev-fwd")((x,), vec),
+            (torch.diag(-x.sin()) @ vec.T).T,
+        )
 
     def test_hvp_vmap(self):
         """Test vmap's usage on hvp."""
@@ -109,8 +121,10 @@ class TestIHVP:
         def vmap_on_x(x):
             return hvp(target, argnums=0, mode="rev-rev")((x,), vec)
 
-        torch.allclose(vmap(vmap_on_x)(x),
-                       torch.stack([vmap_on_x(x[i]) for i in range(5)]))
+        torch.allclose(
+            vmap(vmap_on_x)(x),
+            torch.stack([vmap_on_x(x[i]) for i in range(5)]),
+        )
 
     def test_ihvp_cg(self):
         """Test ihvp_cg/ihvp_cg_at_x."""
@@ -122,12 +136,18 @@ class TestIHVP:
         vec = torch.randn(5, 2)
         ihvp = ihvp_at_x_cg(target, x, argnums=0)
 
-        assert torch.allclose(ihvp(vec),
-                              (torch.diag(-1 / x.sin()) @ vec.T).T,
-                              rtol=1e-04, atol=1e-07)
-        assert torch.allclose(ihvp_cg(target, argnums=0)((x,), vec),
-                              (torch.diag(-1 / x.sin()) @ vec.T).T,
-                              rtol=1e-04, atol=1e-07)
+        assert torch.allclose(
+            ihvp(vec),
+            (torch.diag(-1 / x.sin()) @ vec.T).T,
+            rtol=1e-04,
+            atol=1e-07,
+        )
+        assert torch.allclose(
+            ihvp_cg(target, argnums=0)((x,), vec),
+            (torch.diag(-1 / x.sin()) @ vec.T).T,
+            rtol=1e-04,
+            atol=1e-07,
+        )
         assert ihvp(vec).shape == (5, 2)
 
     def test_ihvp_cg_argnum(self):
@@ -141,39 +161,62 @@ class TestIHVP:
         vec = torch.randn(5, 2)
         ihvp = ihvp_at_x_cg(target, x, y, argnums=1)
 
-        assert torch.allclose(ihvp(vec),
-                              (torch.diag(-1 / (1 + y).sin()) @ vec.T).T,
-                              rtol=1e-04, atol=1e-07)
-        assert torch.allclose(ihvp_cg(target, argnums=1)((x, y), vec),
-                              (torch.diag(-1 / (1 + y).sin()) @ vec.T).T,
-                              rtol=1e-04, atol=1e-07)
+        assert torch.allclose(
+            ihvp(vec),
+            (torch.diag(-1 / (1 + y).sin()) @ vec.T).T,
+            rtol=1e-04,
+            atol=1e-07,
+        )
+        assert torch.allclose(
+            ihvp_cg(target, argnums=1)((x, y), vec),
+            (torch.diag(-1 / (1 + y).sin()) @ vec.T).T,
+            rtol=1e-04,
+            atol=1e-07,
+        )
         assert ihvp(vec).shape == (5, 2)
 
     def test_ihvp_cg_nn(self):
         """Test ihvp_at_x_cg and ihvp_cg for a nn forwarding function ."""
         # create a simple model with example data
-        model = torch.nn.Sequential(torch.nn.Linear(3, 3),
-                                    torch.nn.ReLU(),
-                                    torch.nn.Linear(3, 1))
+        model = torch.nn.Sequential(
+            torch.nn.Linear(3, 3),
+            torch.nn.ReLU(),
+            torch.nn.Linear(3, 1),
+        )
         data = (torch.randn(3), torch.randn(1))
         model.eval()
 
         @flatten_func(model, param_num=0)
         def f(params):
             yhat = torch.func.functional_call(model, params, data[0])
-            return torch.mean((yhat - data[1])**2)
+            return torch.mean((yhat - data[1]) ** 2)
 
         model_params = {k: p for k, p in model.named_parameters() if p.requires_grad}
 
         v = torch.ones(16)
         ihvp_cg_func = ihvp_cg(f, argnums=0, regularization=1e-3)
-        ihvp_cg_at_x_func = ihvp_at_x_cg(f, flatten_params(model_params),
-                                         argnums=0, regularization=1e-3)
-        ihvp_explicit_at_x_func = ihvp_at_x_explicit(f, flatten_params(model_params),
-                                                     argnums=0, regularization=1e-3)
+        ihvp_cg_at_x_func = ihvp_at_x_cg(
+            f,
+            flatten_params(model_params),
+            argnums=0,
+            regularization=1e-3,
+        )
+        ihvp_explicit_at_x_func = ihvp_at_x_explicit(
+            f,
+            flatten_params(model_params),
+            argnums=0,
+            regularization=1e-3,
+        )
 
-        assert torch.allclose(ihvp_cg_at_x_func(v), ihvp_explicit_at_x_func(v),
-                              rtol=1e-03, atol=1e-07)
-        assert torch.allclose(ihvp_cg_func((flatten_params(model_params),), v),
-                              ihvp_explicit_at_x_func(v),
-                              rtol=1e-03, atol=1e-07)
+        assert torch.allclose(
+            ihvp_cg_at_x_func(v),
+            ihvp_explicit_at_x_func(v),
+            rtol=1e-02,
+            atol=1e-05,
+        )
+        assert torch.allclose(
+            ihvp_cg_func((flatten_params(model_params),), v),
+            ihvp_explicit_at_x_func(v),
+            rtol=1e-02,
+            atol=1e-05,
+        )
