5b2aeaabeefeb0e129a8593f8277c656a1538dd6
Ting Wei <litim990357@gmail.com>
2024-04-29 13:42:28 -0500

    - create maestro folder as dattri/benchmark/maestro/ - complete the migrate of MusicTransformer_PyTorch

--- dattri/benchmark/maestro.py
+++
@@ -1,5 +0,0 @@
-"""This module contains functions for model training/evaluation on MAESTRO dataset.
-
-The code is mainly adapted from https://github.com/gwinndr/MusicTransformer-Pytorch.
-The author is Damon Gwinn.
-"""
