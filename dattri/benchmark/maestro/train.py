1775118e18d718213a1fcbfa860676b4eb12735c
Ting Wei <litim990357@gmail.com>
2024-04-29 13:52:20 -0500

    - add __init__ - modify train docstring

---
+++ dattri/benchmark/maestro/train.py
@@ -0,0 +1,5 @@
+"""This module contains functions for model training on MAESTRO dataset.
+
+The code is mainly adapted from https://github.com/gwinndr/MusicTransformer-Pytorch.
+The author is Damon Gwinn.
+"""
