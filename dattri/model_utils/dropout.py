b83ce1c98b82f693d598b5d10779e24cb187533e
Ting Wei <litim990357@gmail.com>
2024-05-06 19:56:37 -0500

    - allow specifying dropout prob - add test file for testing

--- dattri/model_utils/dropout.py
+++ dattri/model_utils/dropout.py
@@ -2,10 +2,10 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
-    from typing import List, Optional
+    from typing import List
 
     import torch
 
@@ -14,7 +14,6 @@ if TYPE_CHECKING:
 # just input the place we want to put, can support "all" and "specific"
 def activate_dropout(
     model: torch.nn.Module,
-    mode: str = "all",
     layer_positions: Optional[List[str]] = None,
     dropout_prob: float = 0.1,
 ) -> None:
@@ -22,29 +21,36 @@ def activate_dropout(
 
     Args:
         model (torch.nn.Module): The model to be used.
-        mode (str): Where to active dropouts. Can be "all" or "specific".
-            Defaults to "all".
         layer_positions (Optional[List[str]]): Specific layer positions to
-            activate dropouts. Should be a list of layer names. Defaults to None.
+            activate dropouts. Should be a list of layer names. Default to
+            all dropout layers found in the model.
         dropout_prob (float): The dropout probability to be applied. If not
             specified, it will be defaulted as 0.1.
     """
     # evaluate the model
     model.eval()
     # activate dropout layers
-    if mode == "all":
-        for module in model.modules():
-            if module.__class__.__name__.startswith("Dropout"):
-                module.p = dropout_prob
-                module.train()
-
-    elif mode == "specific":
-        for name, module in model.named_modules():
-            for layer_name in layer_positions:
-                # if the layer name (string) in name of modules,
-                # then make dropout module in train mode
-                if layer_name in name and module.__class__.__name__.startswith(
-                    "Dropout",
-                ):
+    if layer_positions is None:
+        layer_positions = []
+    if isinstance(layer_positions, list):
+        if len(layer_positions) == 0:
+            # activate all dropout layers found in the model
+            for module in model.modules():
+                if module.__class__.__name__.startswith("Dropout"):
                     module.p = dropout_prob
                     module.train()
+        else:
+            # activate all dropout layers in given positions
+            for name, module in model.named_modules():
+                for layer_name in layer_positions:
+                    # if the layer name (string) in name of modules,
+                    # then make dropout module in train mode
+                    if layer_name in name and module.__class__.__name__.startswith(
+                        "Dropout",
+                    ):
+                        module.p = dropout_prob
+                        module.train()
+
+    else:
+        msg = "Input should be a list of layer names."
+        raise TypeError(msg)
--- test/dattri/model_utils/test_dropout.py
+++ test/dattri/model_utils/test_dropout.py
@@ -20,17 +20,19 @@ class TestDropout:
                 output_dim=5,
                 dropout_prob=0.5,
             ):
-                """Initialize the model parameters."""
                 super(MLP, self).__init__()
                 self.fc1 = nn.Linear(input_dim, hidden_dim)
                 self.dropout1 = nn.Dropout(p=dropout_prob)
-                self.fc2 = nn.Linear(hidden_dim, output_dim)
+                self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+                self.dropout2 = nn.Dropout(p=dropout_prob)
+                self.fc3 = nn.Linear(hidden_dim, output_dim)
 
             def forward(self, x):
-                """Model forward pass."""
                 x = torch.relu(self.fc1(x))
                 x = self.dropout1(x)
-                return self.fc2(x)
+                x = torch.relu(self.fc2(x))
+                x = self.dropout2(x)
+                return self.fc3(x)
 
         test_data = torch.rand(5, 20)
         model = MLP()
@@ -41,12 +43,17 @@ class TestDropout:
         assert torch.allclose(before_result_1, before_result_2)
 
         # activate all
-        activate_dropout(model, dropout_prob=0.1)
+        activate_dropout(model, dropout_prob=0.2)
 
         after_result = model(test_data)
         assert not torch.allclose(before_result_1, after_result)
 
-        # resume
+        # resume by calling eval again
         model.eval()
         resume_result = model(test_data)
         assert torch.allclose(before_result_1, resume_result)
+
+        # activate specific
+        activate_dropout(model, ["dropout2"], dropout_prob=0.2)
+        spec_result = model(test_data)
+        assert not torch.allclose(before_result_1, spec_result)
