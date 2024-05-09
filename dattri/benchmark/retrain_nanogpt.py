6dd17c87703d5aa291481a7df203f9af08467499
SeanZhang <87859811+SeanZh30@users.noreply.github.com>
2024-05-09 17:09:49 -0500

    fix retrain file and named as retrain_nanogpt.py

---
+++ dattri/benchmark/retrain_nanogpt.py
@@ -0,0 +1,95 @@
+"""This script automates the retraining of nanoGPT with different configurations."""
+
+# ruff: noqa: S306, S602, EXE002, S404, PLW1510
+
+import argparse
+import os
+import subprocess
+import tempfile
+from pathlib import Path
+
+
+def retrain(seed: int,
+            subset_ratio: float,
+            config_path: str,
+            dataset_path: str,
+            save_path: str,
+            partition: list) -> None:
+    """Retrains the nanoGPT model multiple times.
+
+    Args:
+        seed (int): The initial random seed for training, incrementing with each run.
+        subset_ratio (float): Fraction of the dataset to use for training.
+        config_path (str): Path to the model's configuration file.
+        dataset_path (str): Path where the training data is stored.
+        save_path (str): Directory where each model output is saved.
+        partition (list): Range of data subsets, [start_id, end_id, total_subsets].
+    """
+    os.chdir("./models/nanoGPT")
+    config = Path(config_path).read_text(encoding="utf-8").splitlines()
+
+    start_id, end_id, total_num = partition
+    end_id = min(end_id, total_num)
+    # Check subset ratio if satisfy the requirement
+    if subset_ratio <= 0:
+        error_message = "subset_ratio must be non-negative"
+        raise ValueError(error_message)
+    if subset_ratio > 1:
+        error_message = "subset_ratio must smaller or equal to 1"
+        raise ValueError(error_message)
+    seed = seed + start_id - 1
+    for i in range(start_id, end_id):
+        modified_config = []
+        for line in config:
+            if line.startswith("out_dir"):
+                modified_config.append(f"out_dir = '{save_path}/model_{i + 1}'\n")
+            elif line.startswith("seed"):
+                modified_config.append(f"seed = {seed}\n")
+            elif line.startswith("subset_ratio"):
+                modified_config.append(f"subset_ratio = {subset_ratio}\n")
+            elif line.startswith("dataset_path"):
+                modified_config.append(f"dataset_path = '{dataset_path}'\n")
+            else:
+                modified_config.append(line)
+
+        temp_config_path = Path(tempfile.mktemp())
+        temp_config_path.write_text("\n".join(modified_config), encoding="utf-8")
+        command = f"python train.py {temp_config_path}"
+        subprocess.run(command, shell=True)
+
+        temp_config_path.unlink()
+        seed += 1
+
+
+def main() -> None:
+    """Main function to parse arguments and call the retraining function."""
+    parser = argparse.ArgumentParser(
+        description="Retrain models with different configurations.")
+    parser.add_argument("--save_path", type=str, default="output_models",
+                        help="The path to save the retrained model.")
+    parser.add_argument("--data_path", type=str,
+                        default="./dataset/shakespeare_char",
+                        help="Path to the dataset.")
+    parser.add_argument("--config_path", type=str,
+                        default="./models/nanoGPT/config/train_shakespeare_char.py",
+                        help="Path to the training configuration file.")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="The seed for retraining.")
+    parser.add_argument("--partition", type=int, nargs=3,
+                        default=[0, 5, 5],
+                        help="Partition for retraining, format in [start, end, total].")
+    parser.add_argument("--subset_ratio", type=float, default=1.0,
+                        help="Subset ratio of the training data.")
+
+    args = parser.parse_args()
+
+    retrain(args.seed,
+            args.subset_ratio,
+            args.config_path,
+            args.data_path,
+            args.save_path,
+            args.partition)
+
+
+if __name__ == "__main__":
+    main()
