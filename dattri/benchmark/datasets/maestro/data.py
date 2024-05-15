ef7796848aed1f4673a9d50f140d566d5ea213af
tingwl0122 <107650157+tingwl0122@users.noreply.github.com>
2024-05-15 01:24:08 -0400

    [dattri.benchmark] Fix output format of maestro dataset creation (#69)
    * - fix the output format of create_maestro_datasets
    
    * - shrink docstring

--- dattri/benchmark/datasets/maestro/data.py
+++ dattri/benchmark/datasets/maestro/data.py
@@ -3,10 +3,13 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Tuple
+from typing import TYPE_CHECKING
 
-import torch
-from torch.utils.data import DataLoader, SubsetRandomSampler
+if TYPE_CHECKING:
+    from typing import Tuple
+
+    from torch.utils.data import Dataset
+
 
 from dattri.benchmark.models.MusicTransformer.dataset.e_piano import (
     create_epiano_datasets,
@@ -17,33 +20,24 @@ from dattri.benchmark.models.MusicTransformer.dataset.preprocess_midi import (
 
 # Data Processing Hyper-parameters ############
 MAX_SEQUENCE = 256  # Maximum midi sequence to consider
-BATCH_SIZE = 64  # Batch size to use
-N_WORKERS = 1  # Number of threads for the dataloader
+FULL_VERSION = True  # State if the whole dataset will be transversed.
+# Data Processing Hyper-parameters ############
 
 
 def create_maestro_datasets(
     dataset_path: str,
-    train_size: int = 5000,
-    val_size: int = 500,
-    test_size: int = 500,
-    seed: int = 42,
-) -> Tuple[DataLoader, DataLoader, DataLoader]:
-    """Create data loaders for training, validation, and testing.
+) -> Tuple[Dataset, Dataset, Dataset]:
+    """Create MAESTRO dataset.
 
     Args:
         dataset_path (str): Root directory of the MAESTRO Dataset. Should be
             downloaded from https://magenta.tensorflow.org/datasets/maestro#v200.
             The data is called "maestro-v2.0.0-midi.zip". Please unzip the file.
-        train_size (int): The train loader size. Default to 5000.
-        val_size (int): The validation loader size. Default to 500.
-        test_size (int): The test loader size. Default to 500.
-        seed (int): The random seed used to sample these examples.
 
     Returns:
-        A tuple of three DataLoader objects for train/val/test.
+        Tuple[Dataset, Dataset, Dataset]: The training, validate and testing
+            MAESTRO datasets.
     """
-    # set seed
-    torch.manual_seed(seed)
     # read unzipped files and preprocess the files
     processed_path = f"{dataset_path}-processed"
     processed_dir = Path(dataset_path).parent / processed_path
@@ -54,28 +48,7 @@ def create_maestro_datasets(
     train_dataset, val_dataset, test_dataset = create_epiano_datasets(
         processed_path,
         MAX_SEQUENCE,
-        full_version=True,
+        full_version=FULL_VERSION,
     )
 
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=N_WORKERS,
-        sampler=SubsetRandomSampler(range(train_size)),
-    )
-
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=N_WORKERS,
-        sampler=SubsetRandomSampler(range(val_size)),
-    )
-
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=N_WORKERS,
-        sampler=SubsetRandomSampler(range(test_size)),
-    )
-
-    return train_loader, val_loader, test_loader
+    return train_dataset, val_dataset, test_dataset
