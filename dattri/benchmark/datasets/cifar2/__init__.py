46c503f30478a6ba28acc91b4772ed91dcf3cbe4
tingwl0122 <107650157+tingwl0122@users.noreply.github.com>
2024-05-15 00:42:03 -0400

    [dattri.benchmark] Add ResNet-9 benchmark on CIFAR-2 (#68)
    * - add cifar2-resnet9 benchmark files
    - add test file
    
    * - fix ruff format

---
+++ dattri/benchmark/datasets/cifar2/__init__.py
@@ -0,0 +1,8 @@
+"""This module contains functions for model training/evaluation on CIFAR-2 dataset."""
+
+__all__ = [
+    "create_cifar2_dataset",
+    "create_resnet9_model",
+    "loss_cifar2_resnet9",
+    "train_cifar2_resnet9",
+]
