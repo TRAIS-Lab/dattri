e9cc2371d312024759723e369f5e91a3dd412300
Sx Lau <3211220990@qq,com>
2024-06-10 21:43:57 -0400

    Add test conv

--- test/dattri/func/test_ihvp.py
+++ test/dattri/func/test_ihvp.py
@@ -448,6 +448,89 @@ class TestIHVP:
                                estimation[i].detach().numpy())[0, 1]
             assert corr > 0.95  # noqa: PLR2004
 
+    def test_ihvp_ekfac_conv(self):
+        """Test ihvp_ekfac_at_x with conv."""
+        dim_in, dim_out = 100, 1
+        kernel_size = 5
+        sample_size = 5000
+
+        class MixedModel(torch.nn.Module):
+            def __init__(self) -> None:
+                super(MixedModel, self).__init__()
+                self.conv = torch.nn.Conv1d(1, 1, kernel_size, padding=kernel_size // 2)
+                self.linear = torch.nn.Linear(dim_in, dim_out, bias=False)
+
+            def forward(self, x: torch.Tensor) -> torch.Tensor:
+                x = x.view(x.shape[0], 1, -1)
+                x = self.conv(x).view(x.shape[0], -1)
+                return self.linear(x)
+
+        @manual_cache_forward
+        def custom_forward_method(self, hidden_states):
+            if not hasattr(self, EKFAC_CACHE_KEY):
+                # Normal forward pass
+                hidden_states = hidden_states.view(hidden_states.shape[0], -1)
+                return self.conv(self.linear(hidden_states))
+            # Forward pass with caching i/o variables
+            cache = getattr(self, EKFAC_CACHE_KEY)
+            hidden_states = hidden_states.view(hidden_states.shape[0], 1, -1)
+            x1 = self.conv(hidden_states).view(hidden_states.shape[0], -1)
+            y1 = self.linear(x1)
+            cache.input_hidden_pairs.append((x1, y1))
+            return y1
+
+        model = MixedModel()
+        model_params = {k: p for k, p in model.named_parameters() if p.requires_grad}
+
+        x = torch.randn(sample_size, dim_in)
+        y = torch.randn(sample_size, dim_out)
+        v = torch.randn(5, dim_out, dim_in)
+
+        def f(linear_weights, conv_weights, conv_bias):
+            params = {
+                "linear.weight": linear_weights.view(dim_out, dim_in),
+                "conv.weight": conv_weights,
+                "conv.bias": conv_bias,
+            }
+            logits = torch.func.functional_call(model, params, x)
+            return torch.mean((logits - y)**2)
+
+        @flatten_func(model, param_num=0)
+        def f_ekfac(params, x, y):
+            # The custom function should zero out gradient
+            with torch.no_grad():
+                for param in model.parameters():
+                    if param.grad is not None:
+                        param.grad.zero_()
+
+            logits = torch.func.functional_call(model, params, x)
+            return torch.mean((logits - y)**2, dim=-1)
+
+        ihvp_explicit_at_x_func = ihvp_at_x_explicit(
+            f,
+            flatten_params(model_params),
+            argnums=0,
+        )
+
+        ground_truth = ihvp_explicit_at_x_func(v.reshape(5, -1))
+
+        model.forward = types.MethodType(custom_forward_method, model)
+        cache = MLPCache()
+        setattr(model, EKFAC_CACHE_KEY, cache)
+
+        ihvp_at_x_ekfac_func = ihvp_at_x_ekfac(f_ekfac,
+                                               *(flatten_params(model_params),
+                                                 x, y),
+                                               in_dims=(None, 0, 0),
+                                               mlp_cache=cache,
+                                               batch_size=128,
+                                               damping=0.1)
+
+        estimation = ihvp_at_x_ekfac_func([[v]])[0][0]
+        for i in range(5):
+            corr = np.corrcoef(ground_truth[i].detach().numpy(),
+                               estimation[i].detach().numpy())[0, 1]
+            assert corr > 0.95  # noqa: PLR2004
 
 def test_ihvp_datainf():
     """Testing datainf functionality."""
