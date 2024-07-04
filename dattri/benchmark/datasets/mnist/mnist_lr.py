c6e5b0560ce68678d9c6f7b0d5bdd15dff74dd79
tingwl0122 <liti990357@gmail.com>
2024-07-04 04:17:44 -0500

    - add mnist benchmark file - modify mnist loss function

--- dattri/benchmark/datasets/mnist/mnist_lr.py
+++ dattri/benchmark/datasets/mnist/mnist_lr.py
@@ -68,16 +68,15 @@ def loss_mnist_lr(
     Returns:
         The sum of loss of the model on the loader.
     """
-    criterion = nn.CrossEntropyLoss(reduction="sum")
+    criterion = nn.CrossEntropyLoss(reduction="none")
     model = create_lr_model("mnist")
     model.load_state_dict(torch.load(Path(model_path)))
     model.eval()
-    total_loss = 0
-    total_samples = 0
+    model.to(device)
+    loss_list = []
     with torch.no_grad():
         for inputs, labels in dataloader:
             outputs = model(inputs.to(device))
             loss = criterion(outputs, labels.to(device))
-            total_loss += loss.item() * inputs.shape[0]
-            total_samples += inputs.shape[0]
-    return total_loss / total_samples
+            loss_list.append(loss.clone().detach().cpu())
+    return torch.cat(loss_list)
