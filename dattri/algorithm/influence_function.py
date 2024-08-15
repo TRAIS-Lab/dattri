44cd8ca213a3e30f18a710802261e3eb8b96f3c1
yijunp <yijunp@is-markov.ischool.illinois.edu>
2024-08-14 21:31:48 -0500

    Change/Name change

--- dattri/algorithm/influence_function.py
+++ dattri/algorithm/influence_function.py
@@ -210,62 +210,7 @@ class IFAttributorLiSSA(BaseInnerProductAttributor):
             tuple(sampled_input[i].float() for i in range(1, len(sampled_input))),
         )
 
-
-class IFAttributorDataInf(BaseInnerProductAttributor):
-    """The inner product attributor with DataInf inverse hessian transformation."""
-
-    def transformation_on_query(
-        self,
-        index: int,
-        train_data: Tuple[torch.Tensor, ...],
-        query: torch.Tensor,
-        **transformation_kwargs,
-    ) -> torch.Tensor:
-        """Calculate the transformation on the query through ifvp_datainf.
-
-        Args:
-            index (int): The index of the model parameters. This index
-                is used for ensembling of different trained model.
-            train_data (Tuple[Tensor]): The train data. Normally this is a tuple
-                of input data and target data, the number of items in the
-                tuple should be aligned in the target function. The tensors'
-                shape follows (1, batchsize, ...).
-            query (torch.Tensor): The query to be transformed. Normally it is
-                a 2-d dimensional tensor with the shape of
-                (batchsize, num_parameters).
-            transformation_kwargs (Dict[str, Any]): The keyword arguments for
-                the transformation function.
-
-        Returns:
-            torch.Tensor: The transformation on the query. Normally it is a 2-d
-                dimensional tensor with the shape of (batchsize, transformed_dimension).
-        """
-        from dattri.func.fisher import ifvp_datainf
-        model_params, param_layer_map = self.task.get_param(index, layer_split=True)
-        self.ihvp_func = ifvp_datainf(
-            self.task.get_loss_func(),
-            0,
-            (None, 0),
-            param_layer_map=param_layer_map,
-            **transformation_kwargs,
-        )
-        split_index = [0] * (param_layer_map[-1] + 1)
-        for idx, layer_index in enumerate(param_layer_map):
-            split_index[layer_index] += model_params[idx].shape[0]
-
-        current_idx = 0
-        query_split = []
-        for i in range(len(split_index)):
-            query_split.append(query[:, current_idx : split_index[i] + current_idx])
-            current_idx += split_index[i]
-
-        res = self.ihvp_func(
-            (model_params, (train_data[0], train_data[1].view(-1, 1).float())),
-            query_split,
-        )
-        return torch.cat(res, dim=1).detach()
-
-class DataInfAttributor(BaseAttributor):
+class IFAttributorDataInf(BaseAttributor):
     """The base class for inner product attributor."""
 
     def __init__(
