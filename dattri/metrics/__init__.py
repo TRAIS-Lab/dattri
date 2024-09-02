fa03ada51c9b22068bab2b6f488d7a3a327f0f92
jiaqima <jiaqima.mle@gmail.com>
2024-09-02 19:18:33 -0400

    roll back metrics import

--- dattri/metrics/__init__.py
+++ dattri/metrics/__init__.py
@@ -1,6 +1,1 @@
 """dattri.metrics for some metric functions on data attribution."""
-
-from .brittleness import brittleness
-from .metrics import lds, loo_corr, mislabel_detection_auc
-
-__all__ = ["brittleness", "lds", "loo_corr", "mislabel_detection_auc"]
