ccae9ddec85968a9a791d1aaacffdafb599649a7
leaf-source <Oscar.Panyue@outlook.com>
2025-06-05 07:20:04 -0400

    [Bug fix] creates only one iterator object instead of creating one every loop (#188)
    * issue178 solve
    
    * issue178 solve
    
    * issue178: replace ’try and expect'

--- dattri/algorithm/influence_function.py
+++ dattri/algorithm/influence_function.py
@@ -525,8 +525,11 @@ class IFAttributorArnoldi(BaseInnerProductAttributor):
         # Assuming that full_train_dataloader has only one batch
         iter_number = math.ceil(len(full_train_dataloader) * self.precompute_data_ratio)
         data_target_pair_list = []
+        iterator = iter(full_train_dataloader)
         for _ in range(iter_number):
-            data_target_pair_list.append(next(iter(full_train_dataloader)))  # noqa: PERF401
+            if (batch := next(iterator, None)) is None:
+                break
+            data_target_pair_list.append(batch)
 
         # concatenate all data
         data_target_pair = data_target_pair_list[0]
@@ -820,8 +823,11 @@ class IFAttributorDataInf(BaseInnerProductAttributor):
                 len(full_train_dataloader) * self.fim_estimate_data_ratio,
             )
             sampled_data_list = []
+            iterator = iter(full_train_dataloader)
             for _ in range(iter_number):
-                sampled_data_list.append(next(iter(full_train_dataloader)))  # noqa: PERF401
+                if (batch := next(iterator, None)) is None:
+                    break
+                sampled_data_list.append(batch)
             for sampled_data_ in sampled_data_list:
                 sampled_data = tuple(
                     data.to(self.device).unsqueeze(0) for data in sampled_data_
