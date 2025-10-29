47264a39b1f3e57eaaf6dbc8d33d1a38d811b4d6
haochend413 <dinghaochen413@gmail.com>
2025-10-29 16:19:56 -0500

    ruff fixes

--- dattri/benchmark/datasets/wikitext2/__init__.py
+++ dattri/benchmark/datasets/wikitext2/__init__.py
@@ -1,6 +1,10 @@
-"""This module contains functions for model training/evaluation on the wikitext dataset."""
+"""This module contains functions for model training/evaluation on wikitext2."""
 
-__all__ = ["create_wikitext2_dataset", "create_wikitext_datasettrain_wikitext_gpt2loss_wikitext_gpt2", "loss_wikitext2_gpt2", "train_wikitext2_gpt2"]
+__all__ = [
+    "create_wikitext2_dataset",
+    "loss_wikitext2_gpt2",
+    "train_wikitext2_gpt2",
+]
 
 from .data import create_wikitext2_dataset
 from .wikitext2_gpt2 import loss_wikitext2_gpt2, train_wikitext2_gpt2
