9879378e4629fc9de0a31ade2ac8c6c92dff0731
BilyHurington <bilyhurington@gmail.com>
2025-10-30 12:21:56 -0400

    update unittest for dvemb

--- test/dattri/algorithm/test_dvemb.py
+++ test/dattri/algorithm/test_dvemb.py
@@ -1,12 +1,16 @@
 """Tests for the Data Value Embedding (DVEmb)."""
 
+import math
+
 import pytest
 import torch
 from torch import nn
+from torch.func import functional_call
 from torch.utils.data import DataLoader, TensorDataset
 
 from dattri.algorithm.dvemb import DVEmbAttributor
 from dattri.benchmark.datasets.mnist import train_mnist_lr
+from dattri.task import AttributionTask
 
 
 class TestDVEmbAttributor:
@@ -27,9 +31,18 @@ class TestDVEmbAttributor:
         self.test_loader = DataLoader(self.test_dataset, batch_size=2, shuffle=False)
 
         self.model = train_mnist_lr(self.train_loader, epoch_num=1)
-        self.device = "cpu"
+        self.criterion = nn.CrossEntropyLoss()
 
-        self.loss_func = nn.CrossEntropyLoss()
+        def functional_loss(params, data):
+            images, labels = data
+            y_hat = functional_call(self.model, params, (images,))
+            return self.criterion(y_hat, labels.unsqueeze(0))
+
+        self.task = AttributionTask(
+            model=self.model,
+            loss_func=functional_loss,
+            checkpoints=[self.model.state_dict()],
+        )
 
     def _run_dvemb_simulation(self, attributor: DVEmbAttributor):
         """A generic simulation runner for any configured DVEmbAttributor."""
@@ -81,9 +94,8 @@ class TestDVEmbAttributor:
     def test_dvemb_no_projection(self):
         """Test DVEmb without random projection."""
         attributor = DVEmbAttributor(
-            model=self.model,
-            loss_func=self.loss_func,
-            device=self.device,
+            task=self.task,
+            criterion=self.criterion,
             factorization_type="none",
         )
         self._run_dvemb_simulation(attributor)
@@ -92,9 +104,8 @@ class TestDVEmbAttributor:
         """Test DVEmb with random projection."""
         proj_dim = 16
         attributor = DVEmbAttributor(
-            model=self.model,
-            loss_func=self.loss_func,
-            device=self.device,
+            task=self.task,
+            criterion=self.criterion,
             proj_dim=proj_dim,
             factorization_type="none",
         )
@@ -105,9 +116,8 @@ class TestDVEmbAttributor:
     def test_dvemb_kronecker_no_projection(self):
         """Test DVEmb with Kronecker factorization and no projection."""
         attributor = DVEmbAttributor(
-            model=self.model,
-            loss_func=self.loss_func,
-            device=self.device,
+            task=self.task,
+            criterion=self.criterion,
             factorization_type="kronecker",
         )
         self._run_dvemb_simulation(attributor)
@@ -117,15 +127,19 @@ class TestDVEmbAttributor:
         """Test DVEmb with Kronecker factorization and projection."""
         proj_dim = 36
         attributor = DVEmbAttributor(
-            model=self.model,
-            loss_func=self.loss_func,
-            device=self.device,
+            task=self.task,
+            criterion=self.criterion,
             factorization_type="kronecker",
             proj_dim=proj_dim,
         )
         self._run_dvemb_simulation(attributor)
         assert attributor.use_factorization
         assert attributor.random_projectors is not None
+
+        num_layers = len(attributor._linear_layers)
+        expected_proj_dim = int(math.sqrt(proj_dim / num_layers))
+        assert attributor.projection_dim == expected_proj_dim
+
         assert (
             attributor.cached_factors[0][0][0]["A"].shape[1]
             == attributor.projection_dim
@@ -135,9 +149,8 @@ class TestDVEmbAttributor:
         """Test DVEmb with elementwise factorization and projection."""
         proj_dim = 16
         attributor = DVEmbAttributor(
-            model=self.model,
-            loss_func=self.loss_func,
-            device=self.device,
+            task=self.task,
+            criterion=self.criterion,
             factorization_type="elementwise",
             proj_dim=proj_dim,
         )
@@ -146,6 +159,23 @@ class TestDVEmbAttributor:
         assert attributor.random_projectors is not None
         grad_dim = attributor.cached_gradients[0][0].shape[1]
         assert grad_dim == attributor.projection_dim
+
+    def test_dvemb_kronecker_with_layer_names(self):
+        """Test DVEmb with Kronecker factorization and specified layer names."""
+        target_layer = "linear"
+        attributor = DVEmbAttributor(
+            task=self.task,
+            criterion=self.criterion,
+            factorization_type="kronecker",
+            layer_names=[target_layer],
+        )
+        assert len(attributor._linear_layers) == 1
+        assert (
+            attributor._linear_layers[0]
+            == dict(self.model.named_modules())[target_layer]
+        )
+        self._run_dvemb_simulation(attributor)
+        assert len(attributor.cached_factors[0][0]) == 1
 
 
 if __name__ == "__main__":
